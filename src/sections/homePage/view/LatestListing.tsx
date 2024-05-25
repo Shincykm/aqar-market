1720f03a2ad947f713d5a4aff7bc03f78748a5c5
shincy <shincy@mvp-apps.ae>
2024-05-25 12:57:12 +0400

    handling missing values

--- src/sections/homePage/view/LatestListing.tsx
+++ src/sections/homePage/view/LatestListing.tsx
@@ -46,73 +46,67 @@ const LatestListing = async () => {
             <Carousel images={property.pictures} />
             {/* Card body */}
             {/* <Link href={"#"}> */}
-              {property?.property_type && (
-                <div className="overlay p-6 bg-transparent max-w-[400px]">
-                  <div className="bg-primary-blue max-w-max px-4 py-2 rounded-lg">
-                    <p className="text-white text-xs">
-                      {property.property_type?.name_en}
-                    </p>
-                  </div>
-                </div>
-              )}
-
-              <div className="flex flex-col gap-3 py-4 border-b-[1px] border-secondary-gray">
-                <div className="min-h-16">
-                  <h1 className="text-xl text-secondary-blue font-medium">
-                    {property?.name_en}
-                  </h1>
-                </div>
-                {property?.address && (
-                  <p className="font-satoshi text-primary-blue">
-                    Location: {`${property?.address?.country?.name}`}
+            {property?.property_type && (
+              <div className="overlay p-6 bg-transparent">
+                <div className="bg-primary-blue max-w-max px-4 py-2 rounded-lg">
+                  <p className="text-white text-xs capitalize">
+                    {property.property_type?.name_en}
                   </p>
-                )}
-                {property?.amount && (
-                  <p className="text-primary-green text-lg font-medium">
-                    From AED {property?.amount}
-                  </p>
-                )}
-
-                <div className="flex font-normal gap-4">
-                  <div className="flex gap-1">
-                    <Image
-                      src="/icons/ic_link.svg"
-                      width={16}
-                      height={16}
-                      alt={`area-${property?.size_sqft}`}
-                      className="w-4 h-4"
-                    />
-                    <span className="text-xs">{`${
-                      property?.size_sqft || 0
-                    } Sq.Ft`}</span>
-                  </div>
-                  <div className="flex gap-1">
-                    <Image
-                      src="/icons/ic_bed.svg"
-                      width={16}
-                      height={16}
-                      alt={`bed-${property?.count_bathrooms}`}
-                      className="w-5 h-5"
-                    />
-                    <span className="text-xs">{`${
-                      property?.count_bedrooms
-                    } bedrooms`}</span>
-                  </div>
-
-                  <div className="flex gap-1">
-                    <Image
-                      src="/icons/ic_accessories.svg"
-                      width={16}
-                      height={16}
-                      alt={`bathrooms-${property?.count_bathrooms}}`}
-                      className="w-4 h-4"
-                    />
-                    <span className="text-xs">{`${
-                      property?.count_bathrooms
-                    } bathrooms`}</span>
-                  </div>
                 </div>
               </div>
+            )}
+
+            <div className="capitalize flex flex-col gap-3 py-4 border-b-[1px] border-secondary-gray">
+              <div className="min-h-16">
+                <h1 className="text-xl text-secondary-blue font-medium">
+                  {property?.name_en || "Property Name Unavailable"}
+                </h1>
+              </div>
+              
+              <p className="font-satoshi text-primary-blue">
+                Location: {`${property?.address?.country?.name || "Unknown"}`}
+              </p>
+
+              <p className={`${property?.amount ? "text-primary-green" : "text-primary-gray" } text-lg font-medium`}>
+                {`${property?.amount ? `From AED ${property?.amount}` : "Contact Agent For Price"}`}
+              </p>
+
+              <div className="flex font-normal gap-4">
+                {property?.size_sqft && (<div className="flex gap-1">
+                  <Image
+                    src="/icons/ic_link.svg"
+                    width={16}
+                    height={16}
+                    alt={`area-${property?.size_sqft}`}
+                    className="w-4 h-4"
+                  />
+                  <span className="text-xs">{`${property?.size_sqft} Sq.Ft`}</span>
+                </div>)}
+                <div className="flex gap-1">
+                  <Image
+                    src="/icons/ic_bed.svg"
+                    width={16}
+                    height={16}
+                    alt={`bed-${property?.count_bathrooms}`}
+                    className="w-5 h-5"
+                  />
+                  <span className="text-xs">{`${property?.count_bedrooms
+                    } bedrooms`}</span>
+                </div>
+
+                <div className="flex gap-1">
+                  <Image
+                    src="/icons/ic_accessories.svg"
+                    width={16}
+                    height={16}
+                    alt={`bathrooms-${property?.count_bathrooms}}`}
+                    className="w-4 h-4"
+                  />
+                  <span className="text-xs">{`${property?.count_bathrooms
+                    } bathrooms`}</span>
+                </div>
+              </div>
+            </div>
             {/* </Link> */}
             {property?.agents[0] && <AgentCards agentData={property?.agents[0]} />}
           </Cards>
